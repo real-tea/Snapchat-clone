b85c676c75f248b9cce854bb2b91480c3ddec353
Akash <akash.0711a@gmail.com>
2022-01-05 15:13:17 +0530

    preview page

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './Preview.css';
+import { useDispatch , useSelector } from "react-redux";
+import { selectCameraImage } from './features/cameraSlice';
+import { useNavigate } from 'react-router-dom';
+import CloseIcon from '@material-ui/icons/Close';
+import resetCameraImage from './features/cameraSlice';
+
 
 const Preview = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const cameraImage = useSelector(selectCameraImage);
+    useEffect(()=>{
+        if (!cameraImage){
+            navigate('/');
+        }
+    },[ cameraImage , navigate ])
+
+    const closePreview = () => {
+        dispatch(resetCameraImage());
+        // navigate('/');
+    }
+
     return (
-        <div>
-            
+        <div className = "previewImage">
+            <h1>Siuuu</h1>
+            <CloseIcon className = "preview__close" onClick = { closePreview }/>
+            <img src = { cameraImage }/>
         </div>
         
 )}
