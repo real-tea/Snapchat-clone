96f4ef095e146bcdbfbc589e902cf8a0926f04a1
Akash <akash.0711a@gmail.com>
2022-01-06 14:30:48 +0530

    preview css and icons

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 import './Preview.css';
 import { useDispatch , useSelector } from "react-redux";
-import { selectCameraImage } from './features/cameraSlice';
+import { selectCameraImage , resetCameraImage } from './features/cameraSlice';
 import { useNavigate } from 'react-router-dom';
 import CloseIcon from '@material-ui/icons/Close';
-import resetCameraImage from './features/cameraSlice';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
+import SendIcon from '@material-ui/icons/Send';
 
 
 const Preview = () => {
@@ -21,14 +28,28 @@ const Preview = () => {
 
     const closePreview = () => {
         dispatch(resetCameraImage());
-        // navigate('/');
+        
     }
 
     return (
-        <div className = "previewImage">
-            <h1>Siuuu</h1>
-            <CloseIcon className = "preview__close" onClick = { closePreview }/>
+        <div className = "previewImage">            
+            <CloseIcon onClick = { closePreview } className = "preview__close"/>
+            <div className = "preview__toolbarRight">
+                <TextFieldsIcon/>
+                <CreateIcon/>
+                <NoteIcon/>
+                <MusicNoteIcon/>
+                <AttachFileIcon/>
+                <CropIcon/>
+                <TimerIcon/>
+            </div>
+
             <img src = { cameraImage }/>
+            <div classname =  "preview__footer">
+                <h2>Send Now</h2>
+                <SendIcon className = "preview__sendIcon"/>
+                
+            </div>
         </div>
         
 )}
